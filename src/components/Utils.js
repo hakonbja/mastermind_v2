1e8e02dc8faa2bcbc7314a608f4cb7edd54ea466
Hakon <32593278+hakonbja@users.noreply.github.com>
2019-10-24 20:43:30 +0200

    refactor getKeyPegs

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,23 +1,25 @@
 exports.getKeyPegs = (colors, secretColors) => {
   let keyPegs = [];
+  let guess = Array.from(colors);
+  let answer = Array.from(secretColors);
 
   // find perfect matches AND
-  // remove colors that have been correctly guessed from colors and secretColors
-  colors.forEach((color, i) => {
-    if (color === secretColors[i]) {
+  // remove colors that have been correctly guessed from guess and answer
+  guess.forEach((color, i) => {
+    if (color === answer[i]) {
       keyPegs.push('white');
       // whiteIndexes.push(i);
-      colors.splice(i, 1, null);
-      secretColors.splice(i, 1, null);
+      guess.splice(i, 1, null);
+      answer.splice(i, 1, null);
       i--;
     }
   })
 
-  // find partial matches (black pegs) AND remove those colors from arrays colors and secretColors
-  colors.forEach((color => {
-    if (color && secretColors.indexOf(color) > -1) {
+  // find partial matches (black pegs) AND remove those colors from arrays guess and answer
+  guess.forEach((color => {
+    if (color && answer.indexOf(color) > -1) {
       keyPegs.push('black');
-      secretColors.splice(secretColors.indexOf(color), 1);
+      answer.splice(answer.indexOf(color), 1);
     }
   }));
 
